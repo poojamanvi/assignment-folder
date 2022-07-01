c6d726901e1511caf4e7a878e9500c78a2405860
Pooja <poojadahiya121001@gmail.com>
2022-07-01 16:09:15 +0530

    first

---
+++ Pattern assignment/Pattern.js
@@ -0,0 +1,107 @@
+// Question-3
+// let count=1
+// for(i=1;i<=5;i++){
+//     let num=''
+//     for(j=1;j<=i;j++){
+//         num+=`${count} `
+//         count++
+//     }
+//     console.log(num)
+// }
+
+// Question-4
+// let a=0;
+// let b=1;
+// for(i=1;i<=5;i++){
+//     let num=''
+//     for(j=1;j<=i;j++){
+//         num+=`${a} `
+//         let c =a+b
+//          a=b
+//          b=c
+
+//     }
+//   console.log(num)
+// }
+
+
+
+// Question-5
+
+
+// let n=3
+// for(i=1;i<=n;i++){
+//     let num=' '
+//     for(j=1;j<=n-i;j++){
+//         num+=' '
+//         for(k=1;k)
+//     }
+// }
+
+
+// Question-2
+// let n=5
+// for(let i=1;i<=n;i++){
+//     let num=' '
+//     for(let j=n;j>i;j--){
+//         num+=' '
+//     }
+//         for(let k=0;k<i*2-1;k++){
+//             if(k===0||k===2*i-2){
+//                 num+=' * ' 
+//             }else{
+//                 num+='  '
+//             }
+//         }        
+// }
+// for(let i=1;i<=n;i++){
+//     let num=' '
+//     for(let j=0;j<i;j--){
+//         num+=' '
+//     }
+//         for(let k=(n-i)*2-1;k>=1;k--){
+//             if(k===1||k===(n-i)*2-1){
+//                 num+=' * ' 
+//             }else{
+//                 num+='  '
+//             }
+        
+//         }
+// }
+// console.log(num)
+
+
+
+
+// Question-2
+// let n =5
+// for(i=1;i<=n;i++){
+//     let num=' '
+//     for(j=1;j<=n;j++){
+//         if(){
+//             num+=' * '
+//         }else{
+//             num+='  '
+//         }
+//     }
+
+//     console.log(num)
+// }
+// 
+// 
+
+
+//question-1
+// let n=5
+// for(i=1;i<=n;i++){
+//     let num='  '
+//     for(j=1;j<=n;j++){
+//         if(i==j || i+j==n+1){
+//             num+=' * '
+//         }else{
+//             num+='   '
+//         }
+//     }
+//   console.log(num)
+// }
+
---
+++ Pattern assignment/readme.md
@@ -0,0 +1,5 @@
+<h1>Output<h1>
+<img src="Capture.PNG" alt="img">
+<img src="Capture-1.PNG" alt="img">
+<img src="Capture-2.PNG" alt="img">
+<img src="Capture-3.PNG" alt="img">
